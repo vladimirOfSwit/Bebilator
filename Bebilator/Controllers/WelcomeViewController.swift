2b3aa47b83cda0c69d97905111e43f280d252ada
vladimirOfSwift <vladimirsavic456@gmail.com>
2024-08-23 11:27:26 +0200

    Fix Bebilendar button design

--- Bebilator/Controllers/WelcomeViewController.swift
+++ Bebilator/Controllers/WelcomeViewController.swift
@@ -14,20 +14,38 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bebilendarBtn.layer.masksToBounds = false
-        bebilendarBtn.layer.shadowRadius = 15.0
-        bebilendarBtn.layer.shadowColor = UIColor.red.cgColor
-        bebilendarBtn.layer.shadowOffset = CGSize(width: -5.0, height: -5.0)
-        bebilendarBtn.layer.shadowOpacity = 1.0
-        bebilendarBtn.layer.cornerRadius = 7
-        bebilendarBtn.layer.borderColor = UIColor.black.cgColor
-        bebilendarBtn.layer.borderWidth = 3.0
-        bebilendarBtn.clipsToBounds = true
+        
+          // Modify the button
+          bebilendarBtn.frame = CGRect(x: 0, y: 0, width: 342, height: 56)
+          bebilendarBtn.backgroundColor = UIColor.white
+          
+          // Round the corners
+          bebilendarBtn.layer.cornerRadius = 28 // Half of the height to make it fully rounded
+          bebilendarBtn.clipsToBounds = true
+        
+          bebilendarBtn.layer.shadowColor = UIColor.black.cgColor
+          bebilendarBtn.layer.shadowOpacity = 0.5
+          bebilendarBtn.layer.shadowOffset = CGSize(width: 0, height: 2)
+          bebilendarBtn.layer.shadowRadius = 4
+          bebilendarBtn.layer.masksToBounds = false
+        
+  
+        
+
+        
+       
     }
+    
+    
+    
     
     @IBAction func bebilatorButtonPressed(_ sender: UIButton) {
         
         performSegue(withIdentifier: K.HOME_VIEW_CONTROLLER_IDENTIFIER, sender: self)
+    }
+    
+    @IBAction func bebilendarButtonPressed(_ sender: UIButton) {
+        print("Bebilator button pressed.")
     }
     
     
