1ec126d4562e144e13160a05ce1a04143e5a6fe5
vladimirOfSwift <vladimirsavic456@gmail.com>
2024-10-08 09:29:53 +0200

    Create a SwitchPeriodsViewController and SwitchPeriodsViewModel

---
+++ Bebilator/Controllers/SwitchingPeriodsVC/SwitchingPeriodsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SwitchingPeriodsViewController.swift
+//  Bebilator
+//
+//  Created by Vladimir Savic on 8.10.24..
+//
+
+import Foundation
---
+++ Bebilator/Controllers/SwitchingPeriodsVC/SwitchingPeriodsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SwitchingPeriodsViewModel.swift
+//  Bebilator
+//
+//  Created by Vladimir Savic on 8.10.24..
+//
+
+import Foundation
