b5b3039bcc5c96bec1f9df2ea60eb8cc44c41325
vladimirOfSwift <vladimirsavic456@gmail.com>
2024-10-18 13:49:56 +0200

    Fix segue on Bebilendar

--- Bebilator/Controllers/Bebilendar/BebilendarViewController.swift
+++ Bebilator/Controllers/Bebilendar/BebilendarViewController.swift
@@ -22,28 +22,24 @@ class BebilendarViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        futureLimitTextfield.delegate = self
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        view.addGestureRecognizer(tapGesture)
     }
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
     
-    @IBAction func calculateSwitchingPeriodsButtonPressed(_ sender: GradientButton) {
-        guard mTextfield.validateGenderTextfield(isEligible: { bebilatorBrain.isEligible(date: $0) }),
-              wTextfield.validateGenderTextfield(isEligible: {bebilatorBrain.isEligible(date: $0)}) else {
-            return
-        }
-        guard futureLimitTextfield.text?.isEmpty == false, futureLimitTextfield.text != Constants.TEXTFIELD_PLACEHOLDER, let futureLimit = Int(futureLimitTextfield.text ?? "No value") else {
-            futureLimitTextfield.placeholder = "Unesite broj"
-            futureLimitTextfield.isError(baseColor: UIColor.gray.cgColor, numberOfShakes: 4, revert: true)
-            return
-        }
-        
-        guard let mText = mTextfield.text, let wText = wTextfield.text else { return }
+    @IBAction func calculateSwitchingPeriodsButtonPressed(_ sender: UIButton) {
+        guard let mText = mTextfield.text, mTextfield.validateGenderTextfield(isEligible: { bebilatorBrain.isEligible(date: $0) }),
+              let wText = wTextfield.text, wTextfield.validateGenderTextfield(isEligible: { bebilatorBrain.isEligible(date: $0) }) else {
+             return
+         }
+        guard futureLimitTextfield.text?.isEmpty == false,
+               futureLimitTextfield.text != Constants.TEXTFIELD_PLACEHOLDER,
+               let futureLimit = Int(futureLimitTextfield.text ?? "No value") else {
+             futureLimitTextfield.placeholder = "Unesite broj"
+             futureLimitTextfield.isError(baseColor: UIColor.gray.cgColor, numberOfShakes: 4, revert: true)
+             return
+         }
         
         guard let mBdayAsDate = mText.toDate(),
               let wBdayAsDate = wText.toDate() else { return }
@@ -53,13 +49,18 @@ class BebilendarViewController: UIViewController {
             print("Year: \(year), Month: \(month), Day: \(day), Gender: \(gender)")
         }
         
-        //performSegue(withIdentifier: Constants.SWITCH_PERIODS_VIEW_CONTROLLER_RESULTS_IDENTIFIER, sender: self)
+        performSegue(withIdentifier: Constants.SWITCH_PERIODS_VIEW_CONTROLLER_RESULTS_IDENTIFIER, sender: self)
     }
     
     func setupUI() {
         mTextfield.addShadowAndRoundedCorners(color: Constants.colorMborder)
         wTextfield.addShadowAndRoundedCorners(color: Constants.colorWborder)
         futureLimitTextfield.addShadowAndRoundedCorners(color: Constants.colorNBorder)
+        
+        futureLimitTextfield.delegate = self
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
         
         mTextfield.leftImage(UIImage(named: "mIconTextfield"), imageWidth: 5, padding: 10)
         wTextfield.leftImage(UIImage(named: "fIconTextfield"), imageWidth: 5, padding: 10)
